017d8e72a2b7f5ac2be1e691d889cffd8b4d07e8
Deeptikanta Panda <pandde@kredifi.net>
2020-08-04 16:42:22 +0530

    Updates

---
+++ precache-manifest.169ab6600d4e1a8bfce7fcb094f3f385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a568476e19ade0f990a1e17b1beecb",
+    "url": "/MovieInfo/index.html"
+  },
+  {
+    "revision": "001d08bd291efbeec2c5",
+    "url": "/MovieInfo/static/css/main.c777a7f2.chunk.css"
+  },
+  {
+    "revision": "c88fde18ba3e54083a2e",
+    "url": "/MovieInfo/static/js/2.3de13250.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/MovieInfo/static/js/2.3de13250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001d08bd291efbeec2c5",
+    "url": "/MovieInfo/static/js/main.a6444ea8.chunk.js"
+  },
+  {
+    "revision": "51f59347b435905a1223",
+    "url": "/MovieInfo/static/js/runtime-main.c0ed0660.js"
+  }
+]);
